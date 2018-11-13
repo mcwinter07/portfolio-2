c94f1c9318436820ceea888748350978624ff841
Michael Winter <mcwinter07@gmail.com>
2018-11-13 16:22:07 +1100

    auto-render nav slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,47 @@
+// vanilla slider JS
 
-const navLinks = document.getElementById('nav-link')
-// const navArray = Array.from(navLinks)
-console.log(navLinks)
-// console.log(navArray)
+const sliderContainer = document.getElementsByClassName('about-slider')[0]
 
-// navLink.addEventListener("click", sayHi())
+console.log(sliderContainer)
+console.log(sliderContainer.classList)
+
+let sliderWidth = sliderContainer.clientWidth
+
+function adjustSliderSize() {
+  sliderWidth = sliderContainer.clientWidth
+  console.log(sliderWidth)
+}
+
+window.onresize = adjustSliderSize
+
+// TODO: renderSliderNodes
+
+  // console.log(.id)
+  renderPopularItems = () => {
+    let navigationDoms = ``
+    document.querySelectorAll('.about-slide').forEach(slide => {
+      console.log(slide)
+      navigationDoms +=
+      `
+      <li class="about-list-item">
+        <input type="checkbox" class="about-checkbox" id="${slide.id}-checkbox"/>
+      </li>
+      `
+    })
+    document.getElementById('slider-nav-container').insertAdjacentHTML('afterbegin', navigationDoms)
+  }
+  
+  renderPopularItems()
+
+
+  // count how many slides
+  // inject nav items for those slinder into "slider-nav" class
+  // populate checkbox id based off slide id
+  // on check 
+  
 
 
 
-// sayHi = () => {
-//   console.log("hi")
-// }
+// const sliderNav = document.getElementsByClassName('slider-nav')[0]
+//offsetlef
 
-
-
-
-
-// getNavLinks = () => {
-//   navLink = document.getElementsByClassName()
-// }
-
-//TODO: create an event listener for each click
-
-// TODO: create a function that will remove the checked status of the checkbox
