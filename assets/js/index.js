ccd2ebde1bd6adcb5a1488cc7bb25f29864ac176
Michael Winter <mcwinter07@gmail.com>
2018-11-21 17:29:52 +1100

    refactor slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,50 +2,74 @@
 
 const sliderContainer = document.getElementsByClassName('about-slider')[0]
 
-console.log(sliderContainer)
-console.log(sliderContainer.classList)
+let mouseDownDetected = false
+let sliderStartX
+let sliderScrollX
 
-let sliderWidth = sliderContainer.clientWidth
+sliderContainer.addEventListener('mousedown', (e) => {
+  mouseDownDetected = true;
+  sliderContainer.classList.add('sliderActive')
+  sliderStartX = e.pageX - sliderContainer.offsetLeft;
+  sliderScrollX = sliderContainer.scrollLeft
+})
 
-function adjustSliderSize() {
-  sliderWidth = sliderContainer.clientWidth
-  console.log(sliderWidth)
-}
+sliderContainer.addEventListener('mouseleave', () => {
+  mouseDownDetected = false
+})
 
+sliderContainer.addEventListener('mouseup', () => {
+  mouseDownDetected = false
+  sliderContainer.classList.remove('sliderActive')
+})
+
+sliderContainer.addEventListener('mousemove', (e) => {
+  if(!mouseDownDetected) return
+  e.preventDefault
+  const mouseDownX = e.pageX - sliderContainer.offsetLeft
+  const distanceFromOrigin = (mouseDownX - sliderStartX) * 2
+  sliderContainer.scrollLeft = sliderScrollX - distanceFromOrigin
+})
+
+// const adjustSliderSize = () => {
+//   let sliderWidth = sliderContainer.clientWidth
+//   sliderWidth = sliderContainer.clientWidth
+//   console.log(sliderWidth)
+// }
+
+
+
+// const renderSliderNavigation = (sectionClassName, destinationID) => {
+//   let navigationDoms = ``
+//   document.querySelectorAll(sectionClassName).forEach(slide => {
+//     console.log(slide)
+//     navigationDoms +=
+//     `
+//     <li class="about-list-item">
+//       <input type="radio" name="about-slider" class="about-radio" id="${slide.id}-radio"/>
+//     </li>
+//     `
+//   })
+//   document.getElementById(destinationID).insertAdjacentHTML('afterbegin', navigationDoms)
+// }
+
+
+// console.log(document.querySelectorAll('.about-slide'))
+// let testEl = document.querySelectorAll('.about-slide')[0]
+// console.log(testEl)
+// testEl.style.left = sliderWidth
+// // TODO: moveSliderPosition
+
+//   // onlick, find the index of the nav, * by the position of the window and apply to the css of the element
+  
+
+//renders nav radio buttons
+renderSliderNavigation('.about-slide','slider-nav-container')
+
+//update the size variable for the slider
 window.onresize = adjustSliderSize
 
 
-// console.log(.id)
 
-// TODO: renderSliderNodes
-renderSliderNavigation = (sectionClassName, destinationID) => {
-  let navigationDoms = ``
-  document.querySelectorAll(sectionClassName).forEach(slide => {
-    console.log(slide)
-    navigationDoms +=
-    `
-    <li class="about-list-item">
-      <input type="radio" name="about-slider" class="about-radio" id="${slide.id}-radio"/>
-    </li>
-    `
-  })
-  document.getElementById(destinationID).insertAdjacentHTML('afterbegin', navigationDoms)
-}
-
-// TODO: moveSliderPosition
-
-  // onlick, find the index of the nav, * by the position of the window and apply to the css of the element
-  
-
-  
-  renderSliderNavigation('.about-slide','slider-nav-container')
-
-
-  // count how many slides
-  // inject nav items for those slinder into "slider-nav" class
-  // populate checkbox id based off slide id
-  // on check 
-  
 
 
 
